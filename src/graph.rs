1b171a06d27d5caa073ac61e610e6dd4d0b7ed5e
Michael Gallagher <michaelgallagher2017@u.northwestern.edu>
2017-04-26 01:53:29 -0500

    tests

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
-use std::io::{BufRead, BufReader, Read, stdin};
-
+use std::io::{BufRead, BufReader, Read, stdin,BufWriter};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Clone)]
 pub struct Node {
@@ -35,11 +36,84 @@ pub fn construct_graph<R: Read>(reader: R) -> HashMap<String, Node> {
 
     graph
 }
+#[cfg(test)]
+mod construct_graph_tests {
+
+    use super::*;
+    /*
+    #[test]
+    fn construct_graph_test() {
+        let mut buffer = BufWriter::new(File::create("foo.txt"));
+        buffer.write("a b d\nb a d\nc\nd c".as_bytes());
+        buffer.flush();
+        let actual = construct_graph("foo.txt");
+        let mut expected: HashMap<String, Node> = HashMap::new();
+        expected.insert("a".to_string(), Node {neighbors: vec!["b".to_string(),"d".to_string()], parent: None, red: false});
+        expected.insert("b".to_string(), Node {neighbors: vec!["a".to_string(),"d".to_string()], parent: None, red: false});
+        expected.insert("c".to_string(), Node {neighbors: vec![], parent: None, red: false});
+        expected.insert("d".to_string(), Node {neighbors: vec!["c".to_string()], parent: None, red: false});
+        assert_eq!(actual, expected);
+
+    }*/
+}
 
 
 pub fn find_path(nodes: Vec<&str>, graph: HashMap<String, Node>) -> Option<String> {
     None
 }
+
+#[cfg(test)]
+mod find_path_tests {
+
+    use super::*;
+
+    #[test]
+    fn path_itself_test() {
+
+        let mut graph: HashMap<String, Node> = HashMap::new();
+        graph.insert("a".to_string(), Node {neighbors: vec!["b".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("b".to_string(), Node {neighbors: vec!["a".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("c".to_string(), Node {neighbors: vec![], parent: None, red: false});
+        graph.insert("d".to_string(), Node {neighbors: vec!["c".to_string()], parent: None, red: false});
+        let actual = find_path(vec!["a","a"], graph);
+        let expected = Some("a".to_string());
+
+        assert_eq!(actual, expected);
+
+    }
+
+    #[test]
+    fn path_single_test() {
+
+        let mut graph: HashMap<String, Node> = HashMap::new();
+        graph.insert("a".to_string(), Node {neighbors: vec!["b".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("b".to_string(), Node {neighbors: vec!["a".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("c".to_string(), Node {neighbors: vec![], parent: None, red: false});
+        graph.insert("d".to_string(), Node {neighbors: vec!["c".to_string()], parent: None, red: false});
+        let actual = find_path(vec!["a","d"], graph);
+        let expected = Some("a d".to_string());
+
+        assert_eq!(actual, expected);
+
+    }
+
+    #[test]
+    fn path_multiple_test() {
+
+        let mut graph: HashMap<String, Node> = HashMap::new();
+        graph.insert("a".to_string(), Node {neighbors: vec!["b".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("b".to_string(), Node {neighbors: vec!["a".to_string(),"d".to_string()], parent: None, red: false});
+        graph.insert("c".to_string(), Node {neighbors: vec![], parent: None, red: false});
+        graph.insert("d".to_string(), Node {neighbors: vec!["c".to_string()], parent: None, red: false});
+        let actual = find_path(vec!["a","c"], graph);
+        let expected = Some("a d c".to_string());
+
+        assert_eq!(actual, expected);
+
+    }
+}
+
+
 
 // impl Hash for Node {
 //     fn hash<H: Hasher>(&self, state: &mut H) {
